6f11d3dae17b133a9c2c0c39945f26ec91f30408
Shaikat roy <shaikatroy258@gmail.com>
2023-05-18 02:21:45 +0600

    router tailwind daisyUI added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  daisyui: {
+    themes: ["corporate"],
+  },
+  plugins: [require("daisyui")],
+}
+
