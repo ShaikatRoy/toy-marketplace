dc2722c92fed78a30f6c8cac1942efc49818c2e8
Shaikat roy <shaikatroy258@gmail.com>
2023-05-21 17:15:56 +0600

    photo gallery and blog added

--- src/pages/Home/Blogs/Blogs.jsx
+++ src/pages/Home/Blogs/Blogs.jsx
@@ -1,10 +1,56 @@
 
 const Blogs = () => {
-    return (
-        <div>
-            <h2>This is Blog Page</h2>
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <div className="max-w-3xl mx-auto">
+        <h2 className="text-2xl font-bold mb-4">Blog</h2>
+
+        <div className="my-8">
+          <h3 className="text-lg font-semibold mb-2">What is an access token and refresh token?</h3>
+          <p>
+            An access token is a credential used to authenticate and authorize requests to access protected resources,
+            such as APIs or web services. It is typically a JSON Web Token (JWT) that contains information about the
+            users identity and permissions. Access tokens have a limited lifespan and need to be refreshed periodically.
+          </p>
+
+          <h3 className="text-lg font-semibold mt-6 mb-2">Compare SQL and NoSQL databases</h3>
+          <p>
+            SQL (Structured Query Language) databases are based on a relational model and use structured schemas with
+            tables, rows, and columns. They are well-suited for structured data and complex queries with joins and
+            transactions. NoSQL (Not Only SQL) databases, on the other hand, are non-relational and provide flexible
+            schema designs. They are suitable for handling unstructured or semi-structured data and can scale
+            horizontally more easily.
+          </p>
+
+          <h3 className="text-lg font-semibold mt-6 mb-2">What is Express.js?</h3>
+          <p>
+            Express.js is a fast and minimalist web application framework for Node.js. It provides a robust set of
+            features for building web applications and APIs, including routing, middleware support, template engines,
+            and more. Express.js simplifies the process of building server-side applications in JavaScript by providing
+            a lightweight and flexible framework.
+          </p>
+
+          <h3 className="text-lg font-semibold mt-6 mb-2">What is Nest.js?</h3>
+          <p>
+            Nest.js is a progressive Node.js framework for building scalable and efficient server-side applications. It
+            combines elements of object-oriented programming (OOP), functional programming (FP), and functional
+            reactive programming (FRP) to create robust and modular applications. Nest.js leverages TypeScript and
+            follows architectural patterns like Dependency Injection (DI) and Decorators to provide a structured and
+            maintainable approach to building server-side applications.
+          </p>
+
+          <h3 className="text-lg font-semibold mt-6 mb-2">What is MongoDB aggregate?</h3>
+          <p>
+            MongoDBs aggregate is a powerful pipeline-based method used for data aggregation and analysis. It allows
+            you to process and transform documents within a collection using a series of stages. Each stage performs
+            different operations, such as filtering, grouping, sorting, and computing aggregate values. The aggregate
+            framework enables you to perform complex data manipulations and obtain customized results from your MongoDB
+            collections.
+          </p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
--- src/pages/Home/PhotoGallery/PhotoGallery.jsx
+++ src/pages/Home/PhotoGallery/PhotoGallery.jsx
@@ -1,10 +1,37 @@
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
+const images = [
+    "https://i.ibb.co/b71NjFr/star-lord-gallery-646655c5df909.jpg",
+    "https://i.ibb.co/s9q4LWj/iron-man-mark-vii-marvel-gallery-5f7e14cfe1b10.jpg",
+    "https://i.ibb.co/vjfpN50/the-mandalorian-and-grogu-deluxe-version-star-wars-gallery-63f79e57cd1ac.jpg",
+    "https://i.ibb.co/Vwkp3J2/golden-armor-wonder-woman-deluxe-dc-comics-gallery-63d969c75b41a.jpg",
+    "https://i.ibb.co/TrnM2S1/batmobile-dc-comics-gallery-63d960c9a7741.jpg/1000/1000",
+    "https://i.ibb.co/f9gKw6G/captain-america-sam-wilson-closed-wings-version-marvel-gallery-61735d4309316.jpg",
+];
 
 const PhotoGallery = () => {
     return (
-        <div className="text-center font-bold text-3">
-            <h2>This is photo Gallery</h2>
-        </div>
+        <>
+            <div className="p-5 mt-5">
+                <h2 className="text-3xl font-bold text-center my-5" data-aos="fade-right">Hot Collection</h2>
+                <ResponsiveMasonry
+                    columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+                >
+                    <Masonry gutter="20px">
+                        {images.map((image, i) => (
+                            <div className="group" key={i}>
+                                <img
+                                    src={image}
+                                    style={{ width: "90%", display: "block" }}
+                                    alt=""
+                                    className="hover:scale-105"
+                                />
+                            </div>
+                        ))}
+                    </Masonry>
+                </ResponsiveMasonry>
+            </div>
+        </>
     );
 };
 
