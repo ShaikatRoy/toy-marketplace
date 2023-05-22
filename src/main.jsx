22c62a8ab3624237aad9ad88e519b5a4d0ece18d
Shaikat roy <shaikatroy258@gmail.com>
2023-05-23 02:44:01 +0600

    theme updated

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <AuthProviders>
           <RouterProvider router={router} />
       </AuthProviders>
-    </React.StrictMode>,
+    </React.StrictMode>
   </div>
 )
