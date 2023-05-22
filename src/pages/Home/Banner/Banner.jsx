20a9901b0beafa7a8b0b253092e3823daeadc05b
Shaikat roy <shaikatroy258@gmail.com>
2023-05-23 02:24:36 +0600

    short order fixed

--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,12 +1,12 @@
 const Banner = () => {
     return (
         <div className="carousel w-full ">
-            <div id="slide1" className="carousel-item relative w-full">
-                <img src="https://i.ibb.co/Jn90f5R/banner.jpg" className="w-full" />
+            <div id="slide1" className="carousel-item relative w-full ">
+                <img src="https://i.ibb.co/Jn90f5R/banner.jpg" className="w-full h-72 md:h-full" />
                 <div className="absolute text-center flex flex-col items-center justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <div className="text-white mt-8 sm:mt-32">
-                        <h2 className="text-4xl font-bold sm:text-5xl">WHO’S LAUGHING NOW?</h2>
-                        <p className="my-4 sm:my-8">All jokes aside, this Bat-Family will have you in stitches. Pre-order the Batman <br /> Who Laughs Diorama by Quantum Mechanix today!</p>
+                    <div className="text-white sm:mt-32">
+                        <h2 className="text-2xl font-bold sm:text-5xl">WHO’S LAUGHING NOW?</h2>
+                        <p className="my-8 text-xs md:text-base  sm:my-8">All jokes aside, this Bat-Family will have you in stitches. Pre-order the Batman <br /> Who Laughs Diorama by Quantum Mechanix today!</p>
                         <button className="btn btn-primary px-8 py-2 btn-xs sm:btn-sm md:btn-md lg:btn-lg">order now</button>
                     </div>
                 </div>
@@ -16,11 +16,11 @@ const Banner = () => {
                 </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full">
-                <img src="https://i.ibb.co/Jzzw5Tt/banner2.jpg" className="w-full" />
+                <img src="https://i.ibb.co/Jzzw5Tt/banner2.jpg" className="w-full h-72 md:h-full" />
                 <div className="absolute text-center flex flex-col items-center justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <div className="text-white mt-8 sm:mt-32">
-                        <h2 className="text-4xl font-bold sm:text-5xl">BY MJOLNIR’S MIGHT!</h2>
-                        <p className="my-4 sm:my-8">Verily, the Son of Asgard is here! Pre-order the Thor: Breaker <br /> of Brimstone Premium Format Figure for your collection today.</p>
+                        <h2 className="text-2xl font-bold sm:text-5xl">BY MJOLNIR’S MIGHT!</h2>
+                        <p className="my-8 text-xs md:text-base  sm:my-8">Verily, the Son of Asgard is here! Pre-order the Thor: Breaker <br /> of Brimstone Premium Format Figure for your collection today.</p>
                         <button className="btn btn-primary px-8 py-2 btn-xs sm:btn-sm md:btn-md lg:btn-lg">order now</button>
                     </div>
                 </div>
@@ -30,11 +30,11 @@ const Banner = () => {
                 </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full">
-                <img src="https://i.ibb.co/9qdyfQy/banner3.jpg" className="w-full" />
+                <img src="https://i.ibb.co/9qdyfQy/banner3.jpg" className="w-full h-72 md:h-full" />
                 <div className="absolute text-center flex flex-col items-center justify-center transform -translate-y-1/2 left-5 right-5 top-1/2">
                     <div className="text-white mt-8 sm:mt-32">
-                        <h2 className="text-4xl font-bold sm:text-5xl">SHAKE YOUR GROOT THING</h2>
-                        <p className="my-4 sm:my-8">He always lends a hand (and arm) to friends. Bring home Hot Toys’ Groot <br /> Sixth Scale Figure from Guardians of the Galaxy Vol. 3!</p>
+                        <h2 className="text-2xl font-bold sm:text-5xl">SHAKE YOUR GROOT THING</h2>
+                        <p className="my-8 text-xs md:text-base  sm:my-8">He always lends a hand (and arm) to friends. Bring home Hot Toys’ Groot <br /> Sixth Scale Figure from Guardians of the Galaxy Vol. 3!</p>
                         <button className="btn btn-primary px-8 py-2 btn-xs sm:btn-sm md:btn-md lg:btn-lg">order now</button>
                     </div>
                 </div>
--- src/pages/MyToys/MyToys.jsx
+++ src/pages/MyToys/MyToys.jsx
@@ -1,104 +1,115 @@
-/* eslint-disable react-hooks/exhaustive-deps */
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../providers/AuthProviders";
 import MyToysRow from "./MyToysRow";
 import Swal from 'sweetalert2';
 import useTitle from "../../hocks/useTitle";
 
-
 const MyToys = () => {
-    const { user } = useContext(AuthContext);
-    const [myToys, setMyToys] = useState([]);
-    const [sortOrder, setSortOrder] = useState('asc');
-    useTitle('Mytoys')
+  const { user } = useContext(AuthContext);
+  const [myToys, setMyToys] = useState([]);
+  const [sortOrder, setSortOrder] = useState('asc');
+  useTitle('Mytoys')
 
-    useEffect(() => {
-        const url = `https://figure-mania-server.vercel.app/toys?email=${user?.email}&sort=${sortOrder}`;
+  useEffect(() => {
+    const url = `https://figure-mania-server.vercel.app/toys?email=${user?.email}&sort=${sortOrder}`;
 
-        fetch(url)
-            .then((res) => res.json())
-            .then((data) => setMyToys(data));
-    }, [sortOrder, user]);
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setMyToys(data));
+  }, [sortOrder, user]);
 
-    const handleDelete = id => {
-        Swal.fire({
-          title: 'Are you sure?',
-          text: 'You are about to delete the toy',
-          icon: 'warning',
-          showCancelButton: true,
-          confirmButtonText: 'Delete',
-          cancelButtonText: 'Cancel',
-          confirmButtonColor: '#d33',
-          cancelButtonColor: '#3085d6'
-        }).then(result => {
-          if (result.isConfirmed) {
-            fetch(`https://figure-mania-server.vercel.app/toys/${id}`, {
-              method: 'DELETE'
-            })
-              .then(res => res.json())
-              .then(data => {
-                console.log(data);
-                if (data.deletedCount > 0) {
-                  Swal.fire(
-                    'Deleted!',
-                    'The toy has been deleted.',
-                    'success'
-                  );
-                  const remaining = myToys.filter(myToy => myToy._id !== id);
-                  setMyToys(remaining);
-                }
-              })
-              .catch(error => {
-                console.error(error);
-              });
-          }
-        });
-      };
-    
-    
-    const toggleSortOrder = () => {
-        const newSortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
-        setSortOrder(newSortOrder);
-    };
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You are about to delete the toy',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Delete',
+      cancelButtonText: 'Cancel',
+      confirmButtonColor: '#d33',
+      cancelButtonColor: '#3085d6'
+    }).then(result => {
+      if (result.isConfirmed) {
+        fetch(`https://figure-mania-server.vercel.app/toys/${id}`, {
+          method: 'DELETE'
+        })
+          .then(res => res.json())
+          .then(data => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+              Swal.fire(
+                'Deleted!',
+                'The toy has been deleted.',
+                'success'
+              );
+              const remaining = myToys.filter(myToy => myToy._id !== id);
+              setMyToys(remaining);
+            }
+          })
+          .catch(error => {
+            console.error(error);
+          });
+      }
+    });
+  };
 
-    return (
-        <div className="my-10">
-            <h1 className="text-3xl font-bold text-center mb-3">Your Toys</h1>
-            <div>
-                <div className="overflow-x-auto w-full">
-                <div className="flex items-center my-5">
-                <button className="btn btn-primary mx-auto" onClick={toggleSortOrder}>Sort Order</button>
-                </div>
+  const toggleSortOrder = () => {
+    const newSortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+    setSortOrder(newSortOrder);
+  };
 
-                    <table className="table w-full">
-                        {/* head */}
-                        <thead>
-                            <tr>
-                                <th>Picture</th>
-                                <th>UserName</th>
-                                <th>Toy Name</th>
-                                <th>Sub-category</th>
-                                <th>Price</th>
-                                <th>Available Quantity</th>
-                                <th>Delete Button</th>
-                                <th>View Details button</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {myToys.map((myToy) => (
-                                <MyToysRow
-                                    key={myToy.key}
-                                    myToy={myToy}
-                                    handleDelete={handleDelete}
-                                ></MyToysRow>
-                            ))}
-                        </tbody>
-                    </table>
-                </div>
-
+  return (
+    <div className="my-10">
+      <h1 className="text-3xl font-bold text-center mb-3">Your Toys</h1>
+      <div>
+        <div className="overflow-x-auto w-full">
+          <div className="flex items-center my-5">
+            <div className="relative inline-flex">
+              <button
+                className="btn btn-primary mx-auto"
+                onClick={toggleSortOrder}
+              >
+                Sort Order
+              </button>
+              <select
+                className=" bg-white"
+                value={sortOrder}
+                onChange={(e) => setSortOrder(e.target.value)}
+              >
+                <option value="asc">Low to High</option>
+                <option value="desc">High to Low</option>
+              </select>
             </div>
+          </div>
+
+          <table className="table w-full">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>Picture</th>
+                <th>UserName</th>
+                <th>Toy Name</th>
+                <th>Sub-category</th>
+                <th>Price</th>
+                <th>Available Quantity</th>
+                <th>Delete Button</th>
+                <th>View Details button</th>
+              </tr>
+            </thead>
+            <tbody>
+              {myToys.map((myToy) => (
+                <MyToysRow
+                  key={myToy.key}
+                  myToy={myToy}
+                  handleDelete={handleDelete}
+                ></MyToysRow>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MyToys;
